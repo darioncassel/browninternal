9db5e0222a6a0d657aa2eece926726f983afeb44
raddar4 <darioncassel@d-172-26-5-195.bootp.Virginia.EDU>
2014-10-24 16:28:10 -0400

    Removed unnecessary files

--- packages/raddar4:csv/package.js
+++
@@ -1,8 +0,0 @@
-Package.describe({
-	summary: "csv-to-collection - load csv and create meteor collection"
-});
-
-Package.on_use(function (api) {
-	Npm.depends({"comma-separated-values": '3.2.1'});
-	api.add_files(['CsvToCollection.js'], ['client', 'server']);
-});
