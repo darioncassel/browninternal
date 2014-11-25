914bd64f0978fae3991d578741187791ad534c2f
raddar4 <darioncassel@d-172-26-5-198.bootp.Virginia.EDU>
2014-11-25 11:05:17 -0500

    Added calendar-based reservation system
    Requires FullCalendar, Momentjs, and Bootbox.

--- index.js
+++ index.js
@@ -204,4 +204,207 @@ if(Meteor.isClient) {
       };
     }
   });
+  Template.Calendar.rendered = function() {
+    function bootboxContent1() {
+      var str= "<div id='newEvent'><p>Title: <input type='text' id='this_title'></input>\
+      <div class='input-append bootstrap-timepicker'></p>\
+        <p>Start: <input id='timepicker1' type='text' class='input-small'>\
+        <span class='add-on'></i></span><br></p>\
+      </div>\
+      <div class='input-append bootstrap-timepicker'>\
+        <p>End: <input id='timepicker2' type='text' class='input-small'>\
+        <span class='add-on'></i></span></p>\
+      </div>\
+      <p>All Day <input type='checkbox' id='isAllDay'></input></p>\
+      <p><input type='radio' value='Tucker' name='poll1'> Tucker  </input>\
+      <input type='radio' value='Smith' name='poll1'> Smith  </input>\
+      <input type='radio' value='Other' name='poll1'> <input type='text' style='width:100px' value='Other' id='isOther'></input></input></p></div>";
+      var object = $('<div/>').html(str).contents();
+      object.find('#timepicker1').timepicker();
+      object.find('#timepicker2').timepicker();
+      object.find('#isAllDay').click(function(){
+        //perhaps grey out the timepicker fields
+      });
+      return object;
+    }
+    function parseTime(t) {
+      var x = new moment();
+      if(t[6]=='P'){
+        x.hours(parseInt(t[0]+t[1])+12);
+        x.minutes(parseInt(t[3]+t[4]));
+      }else {
+        x.hours(parseInt(t[0]+t[1]));
+        x.minutes(parseInt(t[3]+t[4]));
+      }
+      return x;
+    }
+    $('#calendar').fullCalendar({
+      eventSources: [
+        {
+          events: function(start, end, timezone, callback) {
+            callback(CalendarEvents.find().fetch());
+          }
+        }
+      ],
+      eventMouseover: function(calEvent, jsEvent) {
+        if(calEvent.allDay!=undefined && calEvent.allDay==true){
+          var tooltip = '<div class="tooltipevent"><p>'+calEvent.title+'</p>\
+            All day in '+calEvent.location+'.</div>';
+        }else if(moment(calEvent.start).isSame(moment(calEvent.end), 'day')) {
+          var tooltip = '<div class="tooltipevent"><p>'+calEvent.title+'</p>\
+            In '+calEvent.location+' from '+moment(calEvent.start).format('h:mm a')+'\
+            to '+moment(calEvent.end).format('h:mm a')+'.</div>';
+        }else {
+          var tooltip = '<div class="tooltipevent"><p>'+calEvent.title+'</p>\
+            In '+calEvent.location+' from '+moment(calEvent.start).format('ddd')+ ' at ' + moment(calEvent.start).format('h:mm a')+'\
+            to '+moment(calEvent.end).format('ddd') + ' at ' +  moment(calEvent.end).format('h:mm a')+'.</div>';
+        }
+        $("body").append(tooltip);
+        $(this).mouseover(function(e) {
+          $(this).css('z-index', 10000);
+          $('.tooltipevent').fadeIn('500');
+          $('.tooltipevent').fadeTo('10', 1.9);
+        }).mousemove(function(e) {
+          $('.tooltipevent').css('top', e.pageY + 10);
+          $('.tooltipevent').css('left', e.pageX + 20);
+        });
+      },
+      eventMouseout: function(calEvent, jsEvent) {
+        $(this).css('z-index', 8);
+        $('.tooltipevent').remove();
+      },
+      select: function(start, end) {
+        bootbox.dialog({
+          backdrop: false,
+          message: bootboxContent1,
+          title: 'New Event',
+          buttons: {
+            main: {
+              label: 'Reserve',
+              className: 'btn-primary',
+              callback: function(){
+                var eventData; var add=true;
+                if($('#this_title').val()!='') {
+                  var startTime = $("#timepicker1").val().split('');
+                  var endTime = $("#timepicker2").val().split('');
+                  var title = $('#this_title').val().toLowerCase().replace(/([^a-z])([a-z])(?=[a-z]{2})|^([a-z])/g, function(_, g1, g2, g3){return (typeof g1 === 'undefined') ? g3.toUpperCase() : g1 + g2.toUpperCase(); } );
+                  var location; var color;
+                  if($("input[name='poll1']:checked").val()=='Other'){
+                    location = $('#isOther').val();
+                  }else{
+                    location = $("input[name='poll1']:checked").val();
+                  }
+                  if(location=="Tucker"){
+                    color='#3498db';
+                  }else if(location=="Smith") {
+                    color='#27ae60';
+                  }else {
+                    color='#9b59b6';
+                  }
+                  if($("input[id='isAllDay']").is(":checked")){
+                    eventData={
+                      id: $('#this_title').val()+start+end,
+                      title: title,
+                      allDay: true,
+                      start: moment(start._d).add(1, 'days').format('YYYY MM DD'),
+                      end: moment(end._d).format('YYYY MM DD'),
+                      color: color,
+                      location: location,
+                      type: 'created'
+                    };
+                  }else {
+                    eventData={
+                      id: $('#this_title').val()+start+end,
+                      title: title,
+                      start: moment(start._d).add(1, 'days').hours(parseTime(startTime).hours()).minutes(parseTime(startTime).minutes()).format(),
+                      end: moment(end._d).hours(parseTime(endTime).hours()).minutes(parseTime(endTime).minutes()).format(),
+                      color: color,
+                      location: location,
+                      type: 'created'
+                    };
+                  }
+                  if(!$("input[name='poll1']").is(":checked")){
+                    add=false;
+                    swal("Error", "Please select a location!", "error");
+                  }
+                  if(eventData.allDay==undefined && !moment(eventData.start).isBefore(moment(eventData.end))){
+                    add=false;
+                    swal("Error", "The end time must be after the start time!", "error");
+                  }
+                  if(add){
+                    CalendarEvents.insert(eventData);
+                  }
+                  $('#calendar').fullCalendar('refetchEvents');
+                }
+              }
+            }
+          }
+        })
+        $('#calendar').fullCalendar('unselect');
+      },
+      eventClick: function(event){
+        if(event.type=='created' || event.type=='google'){
+        bootbox.dialog({
+          backdrop: false,
+          message: "<p>Modify Event</p>",
+          closeButton: true,
+          buttons: {
+            modify: {
+              label: "Change Title",
+              callback: function() {
+                bootbox.prompt("New Title: ", function(result) {
+                  if(result && event.type=='created'){
+                    CalendarEvents.update({_id: event._id}, {$set: {title: result}});
+                    $('#calendar').fullCalendar('refetchEvents');
+                  }else if(result && event.type=='google'){
+                    google_updateTitle(result, event);
+                  }
+                });
+              }
+            },
+            delete: {
+              label: "Delete",
+              callback: function() {
+                if(event.type=='created'){
+                  CalendarEvents.remove({_id: event._id});
+                }else if(event.type=='google'){
+                  google_deleteEvent(event);
+                }
+                $('#calendar').fullCalendar('refetchEvents');
+              }
+            }
+          }
+        });
+        }
+      },
+      eventDrop: function(event){
+        if(event.type=='created'){
+          if(event.end!=null){
+            CalendarEvents.update({_id: event._id}, {$set: {allDay: event.allDay, start: moment(event.start).format(), end: moment(event.end).format()}});
+          }else {
+            CalendarEvents.update({_id: event._id}, {$set: {allDay: event.allDay, start: moment(event.start).format()}});
+          }
+        }else if(event.type=='google'){
+          google_updateTime(event);
+        }
+      },
+      eventResize: function(event){
+        if(event.type=='created'){
+          CalendarEvents.update({_id: event._id}, {$set: {end: moment(event.end).format()}});
+        }else if(event.type=='google'){
+          google_resizeTime(event);
+        }
+      },
+      header: {
+        left: 'prev, next, today',
+        center: 'title',
+        right: 'month, agendaWeek, agendaDay'
+      },
+      lazyFetching: false,
+      selectable: true,
+      selectHelper: true,
+      weekends: true,
+      editable: true
+    });
+  }
 }
