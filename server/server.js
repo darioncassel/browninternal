cfad3669c9ab5e2c95dfbbdfb1d36c423608f245
raddar4 <darioncassel@Darions-MacBook-Pro.local>
2015-03-12 12:29:37 -0400

    Stylistic Changes

--- server/server.js
+++ server/server.js
@@ -5,43 +5,44 @@ CalendarEvents = new Mongo.Collection("calendarEvents");
 Accounts.emailTemplates.from = "Brown College Website <no-reply@virginia.edu>"
 Accounts.emailTemplates.siteName = "Brown Internal Website"
 
+Meteor.methods({
+  'updateAccounts' : function() {
+    var resData = Residents.find().fetch();
+    for(i=0; i<resData.length; i++){
+      var email = resData[i]["E-mail"];
+      var username = email.split('@')[0];
+      if (Meteor.users.findOne({ "emails.address" : email }) == null) {
+        Accounts.createUser({
+          username : username,
+          email: email,
+          password: randompass(8),
+          profile : {
+            first_name : resData[i]["First Name"],
+            last_name : resData[i]["Last Name"],
+            gender : resData[i].Gender,
+          }
+        });
+      }
+    }
+  },
+  'reservationEmail' : function (text) {
+    check([text], [String]);
+    this.unblock();
+    Email.send({
+      to: 'bc-chat@list.mail.virginia.edu',
+      from: "\"ZemBot 6000\" <BC_reservations@email.virginia.edu>",
+      subject: 'BC Reservations: New Reservation',
+      text: text
+    });
+  }
+});
+
 Meteor.startup(function () {
   UploadServer.init({
     tmpDir: process.env.PWD + '/.uploads/tmp',
     uploadDir: process.env.PWD + '/.uploads/apps',
     checkCreateDirectories: true,
     acceptFileTypes: /(pdf|doc|docx)/i
-  });
-  Meteor.methods({
-    'updateAccounts' : function() {
-      var resData = Residents.find().fetch();
-      for(i=0; i<resData.length; i++){
-        var email = resData[i]["E-mail"];
-        var username = email.split('@')[0];
-        if (Meteor.users.findOne({ "emails.address" : email }) == null) {
-          Accounts.createUser({
-            username : username,
-            email: email,
-            password: randompass(8),
-            profile : {
-              first_name : resData[i]["First Name"],
-              last_name : resData[i]["Last Name"],
-              gender : resData[i].Gender,
-            }
-          });
-        }
-      }
-    },
-    'reservationEmail' : function (text) {
-      check([text], [String]);
-      this.unblock();
-      Email.send({
-        to: 'bc-chat@list.mail.virginia.edu',
-        from: "\"ZemBot 6000\" <BC_reservations@email.virginia.edu>",
-        subject: 'BC Reservations: New Reservation',
-        text: text
-      });
-    }
   });
 });
 
