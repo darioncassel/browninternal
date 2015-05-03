84d546bb968c87f17687859af2f587c7e932c0af
Darion Cassel <darion.cassel@gmail.com>
2015-05-03 12:01:29 -0400

    Added form validation

--- apply.js
+++ apply.js
@@ -3,10 +3,23 @@ if (Meteor.isClient) {
     uploaded: function() {
       return {
         finished: function(index, fileInfo, context) {
-          console.log(fileInfo);
           $('#entry_1253155548').val(fileInfo.name);
-          console.log($('#entry_1253155548').val());
-          $('#ss-submit').show();
+          $('#submit_brown').show();
+          $('#submit_brown').click(function() {
+            var valid = true;
+            if($("#entry_1751668997").val()==""){valid = false;}
+            if($("#entry_1643291388").val()==""){valid = false;}
+            if(!$("input[name='entry.1370385546']").is(":checked")){valid = false;}
+            if($("#entry_1461977651").val()==""){valid = false;}
+            if($("#entry_2078331929").val()==""){valid = false;}
+            if(!$("input[name='entry.1402613261']").is(":checked")){valid = false;}
+            if($("#entry_1253155548").val()==""){valid = false;}
+            if(valid){
+              $("#ss-submit").click();
+            }else {
+              swal("Please fill in all fields.", "", "error");
+            }
+          });
         }
       }
     }
