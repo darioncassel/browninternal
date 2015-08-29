195ae8c0dd3a5abaf797f92350db03e642a1a079
KeanF <kf9zy@virginia.edu>
2015-08-29 18:55:36 -0400

    Revert "Updated styles and added a page for Social links and a placeholder page for Archives"
    This reverts commit 336a435d033d994f3e0922367b7d802cfe828ae1.

--- roster.js
+++ roster.js
@@ -15,7 +15,7 @@ if (Meteor.isClient) {
     $('#resDataTable').dataTable({
       scrollY: 950,
       scrollCollapse: true,
-      "lengthMenu": [25, 50, 100],
+      "lengthMenu": [25],
       "data": residents,
       "columns": [
           { "title": "Portal" },
