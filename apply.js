1754216216e89e0a696512cc2b0d1c9220100dfa
Wylie Wang <ww5ts@virginia.edu>
2015-10-07 19:02:35 -0400

    Fixed Application changes

--- apply.js
+++ apply.js
@@ -3,17 +3,17 @@ if (Meteor.isClient) {
     uploaded: function() {
       return {
         finished: function(index, fileInfo, context) {
-          $('#entry_1253155548').val(fileInfo.name);
+          $('#entry_955211999').val(fileInfo.name);
           $('#submit_brown').show();
           $('#submit_brown').click(function() {
             var valid = true;
-            if($("#entry_1751668997").val()==""){valid = false;}
-            if($("#entry_1643291388").val()==""){valid = false;}
-            if(!$("input[name='entry.1370385546']").is(":checked")){valid = false;}
-            if($("#entry_1461977651").val()==""){valid = false;}
-            if($("#entry_2078331929").val()==""){valid = false;}
-            if(!$("input[name='entry.1402613261']").is(":checked")){valid = false;}
-            if($("#entry_1253155548").val()==""){valid = false;}
+            if($("#entry_1729943190").val()==""){valid = false;}
+            if($("#entry_1352770139").val()==""){valid = false;}
+            if(!$("input[name='entry.257589673']").is(":checked")){valid = false;}
+            if($("#entry_592522003").val()==""){valid = false;}
+            if($("#entry_419154489").val()==""){valid = false;}
+            if(!$("input[name='entry.770152780']").is(":checked")){valid = false;}
+            if($("#entry_955211999").val()==""){valid = false;}
             if(valid){
               $("#ss-submit").click();
             }else {
